ade1549d06a108e910dfde3d429c211318bf7a46
Grzegorz Pabian <greg.pabian@gmail.com>
2014-11-28 10:37:33 +0100

    Removed "deps" property, added dep module static API use example.

--- src/samplecreator.js
+++ src/samplecreator.js
@@ -1,16 +1,17 @@
-CKE.define( [ 'mvc/plugin' ], function( Plugin ) {
+CKE.define( [ 'mvc/plugin', 'plugins!example' ], function( Plugin, example ) {
 	'use strict';
 
 	return Plugin.extend( {
-		deps: [ 'example' ],
-
 		create: function() {
 			console.log( 'create a sample UI for', this.editor );
 
 			this.editor.execute( 'example', 'foo' );
+
+			example.moduleMethod();
 		},
 
 		init: function() {
+			console.log( 'init sample creator' );
 			this.editor.addCreator( 'sample', this );
 		}
 	} );
